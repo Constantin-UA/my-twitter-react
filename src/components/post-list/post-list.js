56ae30e347a3bb3b8082b65fc4f1916064ac5379
Kostiantyn <konstantinsh579@gmail.com>
2021-07-13 13:23:52 +0300

    add onClick like and important

--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -4,14 +4,16 @@ import PostListItem from '../post-list-item';
 
 import './post-list.css';
 
-const PostList = () => {
-	return (
-		<ul className="app-list list-group">
-			<PostListItem />
-			<PostListItem />
-			<PostListItem />
-		</ul>
-	);
+const PostList = ({ posts }) => {
+	const elements = posts.map((post) => {
+		const { id, ...itemProps } = post;
+		return (
+			<li key={id} className="list-group-item">
+				<PostListItem {...itemProps} />
+			</li>
+		);
+	});
+	return <ul className="app-list list-group">{elements}</ul>;
 };
 
 export default PostList;
